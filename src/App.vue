677a3d8508909ab210d4051c3b60245ad084268b
Jorge Castro <jorge.castro.c@gmail.com>
2021-06-24 11:49:08 -0400

    primera version

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
+
   <div id="app">
     <h1>insertar productos</h1>
-    <ComTexto etiquetaProp="nombre" v-model="producto.nombre" /><br>
-    <ComTexto etiqueta-prop="precio" v-model="producto.precio" /><br>
-    <ComTexto etiqueta-prop="categoria" v-model="producto.categoria" /><br>
+    <ComTexto etiquetaProp="nombre" v-model="producto.nombre"/>
+    <br>
+    <ComTexto etiqueta-prop="precio" v-model="producto.precio"/>
+    <br>
+    <ComTexto etiqueta-prop="categoria" v-model="producto.categoria"/>
+    <br>
     <ComBoton @clickDesdeHijo="eventoApp"></ComBoton>
     <br>
-    <img alt="cargando" src="./assets/wait.gif" v-bind:class="clasegif" />
+    <img alt="cargando" src="./assets/wait.gif" v-bind:class="clasegif"/>
     <br>
     <div v-if="cargando">
-      <img alt="cargando" src="./assets/wait.gif" />
+      <img alt="cargando" src="./assets/wait.gif"/>
     </div>
     <hr>
     <ComTabla v-bind:listadoDeProducto="listaDeProductos" v-bind:cargando="cargandoLista"></ComTabla>
@@ -32,58 +36,59 @@ import axios from 'axios';
 import ComTexto from "@/components/ComTexto";
 import ComBoton from "@/components/ComBoton";
 import ComTabla from "@/components/ComTabla";
+
 export default {
   name: 'App',
   mounted() { // cuando la aplicacion esta creada pero no montada (no dibujada)
-      this.cargandoLista=true;
-      axios.get('http://localhost:63342/junio24vue/web/servicio.php?op=listar'
-          ,{headers:{Authorization:'CLAVESECRETA'}}
-      )
-    .then( resultado=>{
-      this.listaDeProductos=resultado.data;
-      localStorage.setItem('listado',JSON.stringify(this.listaDeProductos)); // localStorage solo trabaja con strings.
-      this.cargandoLista=false;
-    });
+    this.cargandoLista = true;
+    axios.get('http://localhost:63342/junio24vue/web/servicio.php?op=listar'
+        , {headers: {Authorization: 'CLAVESECRETA'}}
+    )
+        .then(resultado => {
+          this.listaDeProductos = resultado.data;
+          localStorage.setItem('listado', JSON.stringify(this.listaDeProductos)); // localStorage solo trabaja con strings.
+          this.cargandoLista = false;
+        });
 
-    this.listadoDeMemoriaLocal=JSON.parse(localStorage.getItem('listado'));
+    this.listadoDeMemoriaLocal = JSON.parse(localStorage.getItem('listado'));
   },
   methods: {
     eventoApp() {
       // click en el boton, se llama a este evento.
-      this.cargando=true;
-      this.clasegif='visible';
+      this.cargando = true;
+      this.clasegif = 'visible';
       axios
-      .post('http://localhost:63342/junio24vue/web/servicio.php?op=insertar'
-          ,this.producto
-          ,{headers:{Authorization:'CLAVESECRETA'}}
-      )
-      .then(respuesta => { // si la operacion es correcta
-        console.log(respuesta);
-        this.listaDeProductos.push(this.producto);
-      })
-      .catch(()=>{  // si la operacion falla
-        console.log("error");
-        //console.log(respuesta);
-      })
-      .finally(()=>{
-        this.cargando=false;
-        this.clasegif='invisible';
-      })
+          .post('http://localhost:63342/junio24vue/web/servicio.php?op=insertar'
+              , this.producto
+              , {headers: {Authorization: 'CLAVESECRETA'}}
+          )
+          .then(respuesta => { // si la operacion es correcta
+            console.log(respuesta);
+            this.listaDeProductos.push(this.producto);
+          })
+          .catch(() => {  // si la operacion falla
+            console.log("error");
+            //console.log(respuesta);
+          })
+          .finally(() => {
+            this.cargando = false;
+            this.clasegif = 'invisible';
+          })
 
     }
   },
   data() {
     return {
-      producto:{
-        nombre:""
-        ,precio:0
-        ,categoria:""
+      producto: {
+        nombre: ""
+        , precio: 0
+        , categoria: ""
       },
-      cargandoLista:false,
-      cargando:false,
-      clasegif:'invisible',
-      listaDeProductos:[],
-      listadoDeMemoriaLocal:[]
+      cargandoLista: false,
+      cargando: false,
+      clasegif: 'invisible',
+      listaDeProductos: [],
+      listadoDeMemoriaLocal: []
     }
   },
   components: {
@@ -96,10 +101,11 @@ export default {
 </script>
 
 <style>
-  .invisible {
-    display: none;
-  }
-  .visible {
-    display: inline-block;
-  }
+.invisible {
+  display: none;
+}
+
+.visible {
+  display: inline-block;
+}
 </style>
